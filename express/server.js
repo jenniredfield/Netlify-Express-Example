91f1979d6c7febe3c4fed0fc2eccc4eca5ea41e5
Jen Bird <jen.b@jbcole.co.uk>
2022-11-21 10:39:17 +0000

    created test route hello

--- express/server.js
+++ express/server.js
@@ -1,36 +1,39 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const router = express.Router();
 
 // Link to views folder.
-let views = path.join(__dirname, '../');
+let views = path.join(__dirname, "../");
 
 // Home route.
-router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: views });
+router.get("/", (req, res) => {
+  res.sendFile("index.html", { root: views });
 });
 
 // Other routes.
-router.get('/page1', function(req, res){
-  res.sendFile('page1.html', { root: views });
+router.get("/page1", function (req, res) {
+  res.sendFile("page1.html", { root: views });
 });
-router.get('/page2', function(req, res){
-  res.sendFile('page2.html', { root: views });
+router.get("/page2", function (req, res) {
+  res.sendFile("page2.html", { root: views });
 });
-router.get('/page3', function(req, res){
-  res.sendFile('page3.html', { root: views });
+router.get("/page3", function (req, res) {
+  res.sendFile("page3.html", { root: views });
 });
-router.get('/page4', function(req, res){
-  res.sendFile('page4.html', { root: views });
+router.get("/page4", function (req, res) {
+  res.sendFile("page4.html", { root: views });
 });
 
+router.get("/hello", function (req, res) {
+  res.send({ message: "just some json" });
+});
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
+app.use("/.netlify/functions/server", router); // path must route to lambda (express/server.js)
 
 module.exports = app;
 module.exports.handler = serverless(app);
